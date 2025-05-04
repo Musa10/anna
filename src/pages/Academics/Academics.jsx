69190bce223f8476b2816aea8364d110585eb72a
annaba.ahmad <annaba.ahmad@infostrategic.com>
2025-05-04 17:29:37 +0300

    add(arch) the new arch

---
+++ src/pages/Academics/Academics.jsx
@@ -0,0 +1,11 @@
+import LearnFeatures from '../../sections/LearnFeatures/LearnFeatures'
+import SpecialFeatures from '../../sections/SpecialFeatures/SpecialFeatures'
+
+export default function Academics() {
+  return (
+    <>
+      <SpecialFeatures />
+      <LearnFeatures />
+    </>
+  )
+}
---
+++ src/sections/LearnFeatures/LearnFeatures.jsx
@@ -0,0 +1,15 @@
+import SectionHeader from "../../components/SectionHeader/SectionHeader";
+import ImgCard from "../../";
+
+export default function LearnFeatures() {
+  return (
+    <section>
+        <SectionHeader headerText="What Students Learn" badgeText="Our Features" descriptionText="At Little Learners Academy, we strive to cultivate a love for learning and equip children with essential skills for their future success. Our academic programs cover a wide range of subjects, allowing students to develop a strong foundation and discover their interests. Some key areas of learning include"/>
+        <div>
+            {cardData.map((card,index)=>
+                <ImgCard />
+            )}
+        </div>
+    </section>
+  )
+}
